7d050846cb9af47818be8664f3b8d6f8d0a4a849
sin <must.sow.before.you.can.reap+0001@gmail.com>
2021-11-29 10:06:28 +0900

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const customSort = require('./lib/sort');
+const customSort = require('./build/custom-sorts');
 module.exports = customSort;
 //const result = customSort.neSort([{a:10},{a:5},{a:20},{a:8}],'a',true);
 //console.log(result)
